e059bc90fc0cc8ef58326afbab0e3b7aac4fe386
Pablovem <pablovema@gmail.com>
2021-12-07 19:28:50 -0500

    feat(react-mir): add Footer component

---
+++ packages/react-mir/src/Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class Footer extends React.Component {
+  render() {
+    return (
+      <footer>
+        <span>Footer, Make it Real, 2021</span>
+      </footer>
+    );
+  }
+}
+
+export default Footer;
