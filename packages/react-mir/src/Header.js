46ad66e2594e5281ae6acc6aea6826041306528f
Pablovem <pablovema@gmail.com>
2021-12-13 18:28:09 -0500

    refactor(react-mir): update Header component

--- packages/react-mir/src/Header.js
+++ packages/react-mir/src/Header.js
@@ -1,17 +1,14 @@
 import React from "react";
 import Navbar from "./Navbar";
 
-class Header extends React.Component {
-  render() {
-    const routes = this.props.routes;
-    return (
-      <header>
-          <span id='company-name'>Make It Real</span>
-          <span className='flex'></span>
-          <Navbar routes={routes} />
-        </header>
-    );
-  }
+function Header(props) {
+  return (
+    <header>
+      <span id="company-name">Make It Real</span>
+      <span className="flex"></span>
+      <Navbar routes={props.routes} />
+    </header>
+  );
 }
 
 export default Header;
