00476a08bf0f63fccdd6816bb981de0d68d1fc81
Pablovem <pablovema@gmail.com>
2021-12-20 19:24:44 -0500

    feat(react-mir): update App and Team components

--- packages/react-mir/src/App.js
+++ packages/react-mir/src/App.js
@@ -1,33 +1,12 @@
 import React from 'react';
-import { Routes, Route, Link } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 // Components
 import Header from './Header';
 import Home from './Home';
 import Team from './Team';
 import Footer from './Footer';
-// Dummy Data
-import mentors from './mentors';
 
 class App extends React.Component {
-  state = {
-    featured: '',
-    mentors: mentors
-  };
-
-  featureProfile = (name) => {
-    this.setState({
-      featured: name
-    });
-  }
-
-  deleteProfile = (profile) => {
-    this.setState((prevState) => ({
-      mentors: prevState.mentors.filter((p) => {
-        return p.id !== profile.id;
-      })
-    }));
-  }
-
   render() {
     const routes = [
       { name: 'Home', url: '/' },
@@ -36,20 +15,9 @@ class App extends React.Component {
       { name: 'Contact', url: '/contact' }
     ];
 
-    const students = [
-      { id: 4, name: 'Maray Montes De Oca' },
-      { id: 5, name: 'Giantory Espino' },
-      { id: 6, name: 'Carlos Sucre' },
-    ];
-
     return (
       <div className='App'>
         <Header company='Make It Real' routes={routes} />
-        <hr></hr>
-        <h2>Links:</h2>
-        <Link to='/'>Go Home</Link>
-        <Link to='team'>Go Team</Link>
-        <hr></hr>
         <Routes>
           <Route path='/' element={ <Home /> } />
           <Route path='team' element={ <Team /> }/>
--- packages/react-mir/src/Team.js
+++ packages/react-mir/src/Team.js
@@ -1,14 +1,40 @@
 import React, { Component } from "react";
 import ContactList from './ContactList';
 import MentorList from './MentorList';
+// Dummy Data
+import mentors from './mentors';
 
 class Team extends Component {
+  state = {
+    featured: '',
+    mentors: mentors
+  };
+
+  featureProfile = (name) => {
+    this.setState({
+      featured: name
+    });
+  }
+
+  deleteProfile = (profile) => {
+    this.setState((prevState) => ({
+      mentors: prevState.mentors.filter((p) => {
+        return p.id !== profile.id;
+      })
+    }));
+  }
+
   render() {
-    const { students } = this.props;
+    const students = [
+      { id: 4, name: 'Maray Montes De Oca' },
+      { id: 5, name: 'Giantory Espino' },
+      { id: 6, name: 'Carlos Sucre' },
+    ];
+
     return (
       <main>
         <h1>Make it Real! Team</h1>
-        {/* <section className="box">
+        <section className="box">
           <h2>Mentors</h2>
           <h3>Featured Mentor: {this.state.featured}</h3>
           <div className="mentors-container">
@@ -21,8 +47,8 @@ class Team extends Component {
         </section>
         <section className="box">
           <h2>Students</h2>
-          <ContactList contacts={students} title={"MIR Students"} />
-        </section> */}
+          <ContactList contacts={students} title='MIR Students' />
+        </section>
       </main>
     );
   }
