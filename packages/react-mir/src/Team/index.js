7a51fc2b6752374acdd4b6190f2361d89f75e368
Pablovem <pablovema@gmail.com>
2022-01-19 21:34:45 -0500

    refactor(react-mir): refactor Team component
    update component to functional

--- packages/react-mir/src/Team/index.js
+++ packages/react-mir/src/Team/index.js
@@ -1,76 +1,58 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from 'react';
 import TextInputWithFocusButton from '../TextInputWithFocusButton/';
 import ContactList from '../ContactList/';
 import MentorList from '../MentorList/';
 
-class Team extends Component {
-  state = {
-    featured: '',
-    mentors: [],
-    students: []
-  };
+const Team = () => {
+  const [featured, setFeatured] = useState('');
+  const [mentors, setMentors] = useState([]);
+  const [students, setStudents] = useState([]);
 
-  componentDidMount() {
-    this.getMentors();
-    this.getStudents();
-  }
+  useEffect(() => {
+    getMentors();
+    getStudents();
+  }, []) 
 
-  getMentors() {
-    // const url = 'http://localhost:3000/mentors.json';
-    const url = 'https://raw.githubusercontent.com/makeitrealcamp/top-v11/develop/packages/react-mir/public/mentors.json';
+  const getMentors = () => {
+    const url = 'http://localhost:3000/mentors.json';
+    // const url = 'https://raw.githubusercontent.com/makeitrealcamp/top-v11/develop/packages/react-mir/public/mentors.json';
     fetch(url)
       .then(response => response.json())
-      .then(data => {
-        this.setState({ mentors: data })
-      })
-      .catch(error => {
-        console.log(error);
-      });;
+      .then(mentors => setMentors(mentors))
+      .catch(error => console.log(error));
   }
 
-  getStudents() {
+  const getStudents = () => {
     fetch('http://hp-api.herokuapp.com/api/characters/students')
       .then(response => response.json())
-      .then(data => this.setState({ students: data }));
+      .then(students => setStudents(students));
   }
 
-  featureProfile = (name) => {
-    this.setState({
-      featured: name
-    });
+  const deleteProfile = (profile) => {
+    setMentors(mentors.filter(p => p.id !== profile.id));
   }
-
-  deleteProfile = (profile) => {
-    this.setState((prevState) => ({
-      mentors: prevState.mentors.filter((p) => {
-        return p.id !== profile.id;
-      })
-    }));
-  }
-
-  render() {
-    return (
-      <main>
-        <h1>Make it Real! Team</h1>
-        <TextInputWithFocusButton />
-        <section className="box">
-          <h2>Mentors</h2>
-          <h3>Featured Mentor: {this.state.featured}</h3>
-          <div className="mentors-container">
-            <MentorList
-              mentors={this.state.mentors}
-              onFeatureProfile={this.featureProfile}
-              onDeleteProfile={this.deleteProfile}
-            />
-          </div>
-        </section>
-        <section className="box">
-          <h2>Students</h2>
-          <ContactList contacts={this.state.students} title='MIR Students' />
-        </section>
-      </main>
-    );
-  }
+  
+  return (
+    <main>
+      <h1>Make it Real! Team</h1>
+      <TextInputWithFocusButton />
+      <section className='box'>
+        <h2>Mentors</h2>
+        <h3>Featured Mentor: {featured}</h3>
+        <div className='mentors-container'>
+          <MentorList
+            mentors={mentors}
+            onFeatureProfile={(name) => setFeatured(name)}
+            onDeleteProfile={(profile) => deleteProfile(profile)}
+          />
+        </div>
+      </section>
+      <section className='box'>
+        <h2>Students</h2>
+        <ContactList contacts={students} title='MIR Students' />
+      </section>
+    </main>
+  );
 }
 
 export default Team;
