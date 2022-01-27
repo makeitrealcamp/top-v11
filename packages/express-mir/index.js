6f4f4b8f4e2ca80597510cd45167078f93912e43
Pablovem <pablovema@gmail.com>
2022-01-26 20:57:11 -0500

    feat(express-mir): update index.js
    setup lowdb

--- packages/express-mir/index.js
+++ packages/express-mir/index.js
@@ -1,7 +1,14 @@
 const express = require('express');
+const lowdDB = require('lowdb');
+const FyleSync = require('lowdb/adapters/FileSync');
 
 // Server Setup
 const PORT = 3000;
+
+// Database Setup
+const adapter = new FyleSync('db.json');
+const db = lowdDB(adapter);
+db.defaults({ }).write();
 
 // Express Setup
 const app = express();
