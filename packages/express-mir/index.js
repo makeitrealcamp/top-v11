b6db6ff683f41f8b065d37d226a8058272ed5a8e
Pablovem <pablovema@gmail.com>
2022-01-26 21:42:09 -0500

    feat(express-mir): update index.js
    add endpoint to create new student

--- packages/express-mir/index.js
+++ packages/express-mir/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const lowdDB = require('lowdb');
 const FyleSync = require('lowdb/adapters/FileSync');
+const joi = require('joi');
+const { nanoid } = require('nanoid');
 
 // Server Setup
 const PORT = 3000;
@@ -13,12 +15,46 @@ db.defaults({ students: [] }).write();
 // Express Setup
 const app = express();
 
+// Global Middlewares
+app.use(express.json());
+
 /** Endpoints */ 
 // Resource: Students
 // GET: fetch students
 app.get('/api/students', (req, res) => {
   const students = db.get('students').value(); // query
   res.status(200).json({ success: true, data: students })
+});
+
+// POST: create new student
+app.post('/api/students', (req, res) => {
+  // get body from request
+  const body = req.body;
+  // validate body
+  const studentSchema = joi.object({
+    name: joi.string().min(3).max(45).required(),
+    surname: joi.string().min(5).max(45).required(),
+    phone: joi.string().length(9).pattern(/^[0-9]+$/).required(),
+    email: joi.string().email().required(),
+    age: joi.number()
+  });
+  const result = studentSchema.validate(body);
+  const { value, error } = result;
+  // if there is no error, body is considered valid
+  if (error == null) {
+    // validation success
+    const student = {
+      _id: nanoid(),
+      ...value,
+      createdAt: Date.now()
+    };
+    // insert into database // mutation - write
+    db.get('students').push(student).write();
+    res.status(200).json({ success: true, message: 'Student has been created', data: student })
+  } else {
+    // validation error
+    res.status(400).json({ success: false, message: 'Validation error', data: value, error: error.details })
+  }
 });
 
 app.listen(PORT, () => {
