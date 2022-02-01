b59bae8c6843e6030da04135b072348b26bbf11f
Pablovem <pablovema@gmail.com>
2022-01-31 20:40:54 -0500

    feat(react-mir): update App component
    update component to funcional and add context

--- packages/react-mir/src/App.js
+++ packages/react-mir/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Routes, Route } from 'react-router-dom';
 // Components
 import Header from './Header/';
@@ -7,33 +7,44 @@ import Team from './Team/';
 import Footer from './Footer/';
 import './App.scss';
 
-class App extends React.Component {
-  render() {
-    const routes = [
-      { name: 'Home', url: '/' },
-      { name: 'Team', url: '/team' },
-      { name: 'Features', url: '/features' },
-      { name: 'Contact', url: '/contact' }
-    ];
+// createContext
+export const CountContext = React.createContext({
+  count: [{ name: 'Home', url: '/' },
+  { name: 'Team', url: '/team' },
+  { name: 'Features', url: '/features' },
+  { name: 'Contact', url: '/contact' }],
+  setCount: () => {}
+});
 
-    return (
-      <div className='App'>
+const routes = [
+  { name: 'Home', url: '/' },
+  { name: 'Team', url: '/team' },
+  { name: 'Features', url: '/features' },
+  { name: 'Contact', url: '/contact' }
+];
+
+const App = () => {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className='App'>
+      <CountContext.Provider value={{ count, setCount }}>
         <Header company='Make It Real' routes={routes} />
-        <Routes>
-          <Route path='/' element={ <Home /> } />
-          <Route path='team' element={ <Team /> }/>
-          <Route 
-            path='*'
-            element={
-              <main>
-                <h2>We weren't able to find what you were looking for</h2>
-              </main>
-            }/>
-        </Routes>
-        <Footer />
-      </div>
-    );
-  }
+      </CountContext.Provider>
+      <Routes>
+        <Route path='/' element={ <Home /> } />
+        <Route path='team' element={ <Team /> }/>
+        <Route 
+          path='*'
+          element={
+            <main>
+              <h2>We weren't able to find what you were looking for</h2>
+            </main>
+          }/>
+      </Routes>
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
