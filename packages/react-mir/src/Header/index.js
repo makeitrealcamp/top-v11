28cb3653c235f7a2f1d77fff747830a279615d74
Pablovem <pablovema@gmail.com>
2022-02-11 23:25:33 -0500

    feat(react-mir): update Header component

--- packages/react-mir/src/Header/index.js
+++ packages/react-mir/src/Header/index.js
@@ -1,24 +1,21 @@
-import React, { useState, useContext } from "react";
+import React, { useContext } from 'react';
 import PropTypes from 'prop-types';
-import Navbar from "../Navbar/";
 import { UserContext, ThemeColors, ThemeContext } from '../App';
+import Navbar from '../Navbar/';
+import ToggleButton from '../ToggleButton/'
 
 const Header = ({ company }) => {
   const { user } = useContext(UserContext);
   const { theme, setTheme } = useContext(ThemeContext);
 
-  const toggleTheme = () => {
-    theme == 'dark' ? setTheme('light'): setTheme('dark')
-  }
-
   return (
     <header style={ ThemeColors[theme] }>
       <span id="company-name" style={ ThemeColors[theme] }>{company}</span>
       <span className="flex"></span>
-      <button onClick={ () => toggleTheme() }>Theme Toggle</button>
+      <Navbar />
       <span className="flex"></span>
       { user.token ? <span>{ user.name } { user.surname }</span> : <span>Not an admin!</span>}
-      <Navbar />
+      <ToggleButton />
     </header>
   );
 }
