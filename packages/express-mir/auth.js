97aa84057452e3718a4700e7f56956e7ac0ccf35
Pablovem <pablovema@gmail.com>
2022-04-11 23:01:03 -0500

    feat(express-mir): update auth.js

--- packages/express-mir/auth.js
+++ packages/express-mir/auth.js
@@ -1,8 +1,9 @@
 const { sign, verify } = require('jsonwebtoken');
-
 const config = require('./config');
-
 const { secret, expires } = config.token;
+const logger = require('./config/logger');
+const mongoose = require('mongoose');
+const { Model } = require('./tasks/model');
 
 const signToken = (payload, expiresIn = expires ) => {
   sign(payload, secret, { 
@@ -37,7 +38,52 @@ const auth = (req, res, next) => {
   }
 }
 
+// middleware me
+const me = (req, res, next) => {
+  const { _id } = req.decoded;
+  const { id } = req.params;
+  if (_id !== id) {
+    res.status(403).json({
+      success: false,
+      message: 'Forbidden'
+    });
+  } else {
+    next();
+  }
+}
+
+// middleware owner 
+const owner = async (req, res, next) => {
+  const { _id } = req.decoded;
+  const { id } = req.params;
+  try {
+    const doc = await Model.findById(id).populate('userId').exec();
+    if (!doc) {
+      const message = `${Model.modelName} not found`;
+      next({
+        message,
+        statusCode: 404,
+        level: 'warn'
+      })
+    } else {
+      const userId = doc.userId.id;
+      if ( _id !== userId) {
+        res.status(403).json({
+          success: false,
+          message: 'Forbidden'
+        });
+      } else {
+        next();
+      }
+    }
+  } catch (err) {
+    next(new Error(err));
+  }
+}
+
 module.exports = {
   signToken,
   auth,
+  me,
+  owner,
 };
